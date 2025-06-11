7a491d9218c84ac2dbff9a367219e56e442bdf5b
RM-Eddy <tantelymicheleddy@gmail.com>
2025-06-11 11:32:43 +0300

    Ajout de .gitignore et mise à jour de fichiers modifiés

--- projet_m1-tect-web/backend/src/main.ts
+++ projet_m1-tect-web/backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = 3025;
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // Remplacez par l'URL de votre frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Si vous avez besoin d'envoyer des cookies avec les requêtes
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Supprime les propriétés non spécifiées dans le DTO
+      forbidNonWhitelisted: true, // Lance une erreur si des propriétés non spécifiées sont envoyées
+      transform: true, // Transforme les types des propriétés des objets en utilisant les décorateurs @Type
+    })
+  );
+  
+  await app.listen(port, () => {
+    console.log(`Le serveur est démarré sur le port ${port}`);
+  });
 }
 bootstrap();
