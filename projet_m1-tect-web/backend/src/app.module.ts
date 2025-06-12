2f6cb0da8407108de88e33656906debd4b6325f2
RM-Eddy <tantelymicheleddy@gmail.com>
2025-06-12 14:51:22 +0300

    modification12juin2025

--- projet_m1-tect-web/backend/src/app.module.ts
+++ projet_m1-tect-web/backend/src/app.module.ts
@@ -1,10 +1,36 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // charge .env
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
+
--- projet_m1-tect-web/backend/src/app.service.ts
+++ projet_m1-tect-web/backend/src/app.service.ts
@@ -3,6 +3,21 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello DY!';
+    // return 'Hello RMDY!';
+    return `
+      <html>
+        <head>
+          <title>Backend-NestJs</title>
+          <style>
+            h2 {
+              color: red;
+            }
+          </style>
+        </head>
+        <body>
+          <h2>Le serveur est démarré sur le port 3025</h2>
+        </body>
+      </html>
+    `;
   }
 }
